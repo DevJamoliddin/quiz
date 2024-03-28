4a465cd7e49ff4f453d27519b375fd319474503b
Jamoliddin Jaloliddinzoda <114978227+DevJamoliddin@users.noreply.github.com>
2024-03-28 11:45:28 +0500

    restart game delays and bounces fixed

--- Assets/BounceEff.cs
+++ Assets/BounceEff.cs
@@ -4,13 +4,34 @@ using DG.Tweening;
 
 public class BounceEff : MonoBehaviour
 {
+    public static bool needToBounce = false;
+    private bool _isBouncing = false;
+
     void Start()
     {
         BounceObj();
     }
 
+    private void Update()
+    {
+        if (needToBounce)
+        {
+            if (!_isBouncing)
+            {
+                BounceObj();
+                DOVirtual.DelayedCall(1f, () => {
+                    needToBounce = false;
+                });
+            }
+        }
+    }
+
     public void BounceObj()
     {
+        _isBouncing = true;
+        DOVirtual.DelayedCall(1.2f, () => {
+            _isBouncing = false;
+        });
         DOTween.Sequence()
         .Append(transform.DOScale(endValue: 1.5f, duration: 0.4f))
         .Append(transform.DOScale(endValue: 0.5f, duration: 0.4f))
--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -6,9 +6,11 @@ public class GameManager : MonoBehaviour
     [SerializeField] private GameObject _level1;
     [SerializeField] private GameObject _level2;
     [SerializeField] private GameObject _level3;
+    [SerializeField] private GameObject _restartPanel;
 
     void Start()
     {
+        _restartPanel.SetActive(false);
         _level1.SetActive(true);
         _level2.SetActive(false);
         _level3.SetActive(false);
@@ -19,24 +21,34 @@ public class GameManager : MonoBehaviour
         DOVirtual.DelayedCall(1.5f, () => {
             if (level == 1)
             {
+                BounceEff.needToBounce = true;
+                _restartPanel.SetActive(false);
                 _level1.SetActive(true);
                 _level2.SetActive(false);
                 _level3.SetActive(false);
             }
             else if (level == 2)
             {
+                BounceEff.needToBounce = true;
                 _level1.SetActive(false);
                 _level2.SetActive(true);
                 _level3.SetActive(false);
             }
             else if (level == 3)
             {
+                BounceEff.needToBounce = true;
                 _level1.SetActive(false);
                 _level2.SetActive(false);
                 _level3.SetActive(true);
             }
         });
+    }
 
+    public void RestartGamePanel()
+    {
+        DOVirtual.DelayedCall(1.5f, () => {
+            _restartPanel.SetActive(true);
+        });
         
     }
 }
